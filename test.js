2b5415cede78d688394125dde5998e08c0ee9543
mmghuneim <muaeim@gmail.com>
2016-03-05 02:17:02 -0500

    Adjusted for scalability
    Accounted for multiple variables being retrieved from the pi

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var myFirebaseRef = new Firebase("https://acmoverwatch.firebaseio.com/");
 
 // Accessing the value when the webpage loads
-myFirebaseRef.once('value', function(dataSnapshot){
+myFirebaseRef.child("system-variables").once('value', function(dataSnapshot){
   // Creates data variable with a snapshot of the database
   var data = dataSnapshot.exportVal();
 
@@ -13,19 +13,29 @@ myFirebaseRef.once('value', function(dataSnapshot){
   else{
     $("#result").html("noone is in the room");
   }
+  $("#result2").html("The time was last updated at " + data.timechanged);
 
 });
 
 // Instantly update the website incase status changes after initial page load
-myFirebaseRef.on("child_changed", function(snapshot) {
+myFirebaseRef.child("system-variables").on("child_changed", function(snapshot) {
   // Store instantaneus status in newOccupied
-  var newOccupied = snapshot.exportVal();
+  var newDataVal = snapshot.exportVal();
+  var snapshotName = snapshot.name();
 
-  // Update status again
-  if(newOccupied ){
-    $("#result").html("someone is in the room");
+  switch(snapshotName){
+    // Update status again
+    case "occupied":
+      if(newDataVal){
+        $("#result").html("someone is in the room");
+      }
+      else{
+        $("#result").html("noone is in the room");
+      }
+      break;
+    case "timechanged":
+      $("#result2").html("The time was updated to " + newDataVal);
+      break;
   }
-  else{
-    $("#result").html("noone is in the room");
-  }
+
 });
