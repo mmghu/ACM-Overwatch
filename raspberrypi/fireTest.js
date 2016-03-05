e83ec6e607fa9881f1a7462add652ea3b8d27c29
Brian Carlson <briancarlson6174@gmail.com>
2016-03-05 17:57:57 -0500

    added a smoother transition of room state variable
    this reduces the number of pushes to the database

--- raspberrypi/fireTest.js
+++ raspberrypi/fireTest.js
@@ -3,22 +3,37 @@ var Firebase = require("firebase");
 var myFirebaseRef = new Firebase("https://acmoverwatch.firebaseio.com/");
 var sysRef = myFirebaseRef.child("system-variables");
 
+var delayTime = 5;
+
 //provides the input that we're using
 var Gpio = require('onoff').Gpio,
     sensor = new Gpio(17, 'in','both');
 
 //function to watch for when button updates
 sensor.watch(function(err, value) {
-  var updateTime =Date.now();//gets the time of the request
-  //value is 0 for false and 1 for true
-  if(value==1){
-    console.log("true");
-    sysRef.update({"occupied":true});//updating data from sysRef
-  }else {
-    console.log("false");
-    sysRef.update({"occupied":false});
-  }
+  var updateTime = Date.now();
+  var previousTime = 0;
 
-  //updates the time of the request to the most recent in the database
-  sysRef.update({"timechanged":updateTime});
+  sysRef.once('value',function(snapshot){
+    previousTime = snapshot.exportVal().timechanged;
+    //only let the value change every 30 seconds max for now
+    if((updateTime-previousTime)/1000>delayTime){
+      console.log((updateTime-previousTime));
+      var previousState = snapshot.exportVal().occupied;
+      //value is 0 for false and 1 for true
+      if(previousState&&value==0){
+        console.log("false");
+        sysRef.update({"occupied":false});
+      }else if (!previousState&&value==1) {
+        console.log("true");
+        sysRef.update({"occupied":true});
+      }
+
+      sysRef.update({"timechanged":updateTime});
+      //updates the log with new entries
+      var roomlogRef=myFirebaseRef.child("roomlog");
+      roomlogRef.push({"value":value,"time":Firebase.ServerValue.TIMESTAMP});
+    }
+  });
+
 });
